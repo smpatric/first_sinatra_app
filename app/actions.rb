8bda0998df75efe68845a9ba28064c8f4c8a6d62
smpatric <patrick.cody.stewart@gmail.com>
2015-03-20 21:37:48 +0000

    added form submission for new post

--- app/actions.rb
+++ app/actions.rb
@@ -9,5 +9,15 @@ get '/messages' do
 end
 
 get '/messages/new' do
-  erb: 'messages/new'
+  erb:'messages/new'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  @message.save
+  redirect '/messages'
 end
